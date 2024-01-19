57ba048a420f3cc699f6305357962d83608c7f6c
MD.SHAHIDUL ISLAM <shahidul.bd.1920@gmail.com>
2024-01-19 20:24:49 +0600

    make hero slider

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,8 @@ import HeroSlider from "@/components/HeroSlider/HeroSlider";
 export default function Home() {
   return (
     <main className="min-h-[calc(100vh-64px)]">
-<<<<<<< HEAD
       <HeroSlider></HeroSlider>
-=======
       <h1 className='text-4xl text-center mt-10'>Home page</h1>
->>>>>>> ed115a5 ([feat](navbar-footer): footer added, navbar bg-color and links updated)
     </main>
   )
 }

879a535863710a0a5e9e994881a94b555a959a9a
MD.SHAHIDUL ISLAM <shahidul.bd.1920@gmail.com>
2024-01-20 23:01:08 +0600

    created hero slider

---
+++ README.md
@@ -0,0 +1,70 @@
+
+# [SyncHome: Smart Residential Building Management System](https://synchome.vercel.app)
+
+**Contributors, Please Be Advised**
+
+## Development Workflow
+
+1. **Directory for Development:**
+   - Developers will work with only the `app` directory.
+
+2. **Branching Strategy:**
+   - Feature/Bugfix branches will be created from the `main` branch.
+   - Branch names should be simple and short. Avoid including sprint data (date, project name, etc.).
+
+3. **Pull Requests:**
+   - After being satisfied with the changes, developers will create a PR to the `development` branch (after deployment, tester will start testing).
+   - The lead will review code changes and merge/close the PR.
+   - Every single feature/bugfix branch will be deleted after a PR.
+
+4. **ReadMe Edits:**
+   - Only lead/Maintainer/operations-team will edit the primary README. The `app` directory can contain README(s) if needed.
+
+5. **Dockerfile:**
+   - Always run the Dockerfile locally before creating a PR to the development branch.
+   - Before starting to code, make sure to pull the development branch.
+   - Before pushing code to GitHub, make sure to pull the development branch.
+
+## Branch and Commit Rules
+
+### Primary Branches:
+- `main`
+- `development`
+
+### Secondary/Feature/Bugfix Branches:
+- Feature: `feature/feature_name`
+- Bugfix: `bugfix/name`
+
+### Commit Messages:
+
+#### Commit Message Format:
+- `[feat](notification): add Eid notification`
+- `[fix](animation): fix rocket animation`
+- `[docs](animation): rocket animation working docs`
+
+#### Pull Request Message Format:
+- `[feat](notification): add Eid notification`
+- `[fix](payment): refactor Bkash payment page`
+
+## Environment Specific URLs
+
+### Backend:
+- **Production:** —-----
+- **Stage:** —--------
+
+### Frontend:
+- **Production:** —-----
+- **Stage:** —-------
+
+## App Environment Variables
+
+- The environment variable files should be named for back-end as `.env` and for front-end `.env.local` only.
+
+Here the front-end env variables are as below: 
+### Firebase credentials
+- `VITE_APIKEY`
+- `VITE_AUTHDOMAIN`
+- `VITE_PROJECTID`
+- `VITE_STORAGEBUCKET`
+- `VITE_MESSAGINGSENDERID`
+- `VITE_APPID`
---
+++ src/components/shared/Navbar.js
@@ -0,0 +1,201 @@
+"use client"
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import logo from '/src/assets/logo.png';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const pages = [
+    {
+        route: 'About Us',
+        pathname: '/about-us'
+    },
+    {
+        route: 'Features',
+        pathname: '/features'
+    },
+    {
+        route: 'Register',
+        pathname: '/register'
+    },
+];
+const settings = [ 'Profile', 'Account', 'Dashboard', 'Logout' ];
+
+function Navbar() {
+    const [ anchorElNav, setAnchorElNav ] = React.useState(null);
+    const [ anchorElUser, setAnchorElUser ] = React.useState(null);
+    const [ user, setUser ] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+        <AppBar position="static" className='bg-white'>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} /> */}
+                    <Box sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }}>
+                        <Image src={logo} height={30} width={30} sx={{ display: { xs: 'none', md: 'flex' } }} alt='logo' />
+                    </Box>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="#"
+                        className='text-black'
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        SyncHome
+                    </Typography>
+
+                    {/* menu for small device */}
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-syncHome"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-syncHome"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                    <Link
+                                        className='text-black uppercase font-mono font-semibold'
+                                        href={page?.pathname}
+                                        textAlign="center">
+                                        {page?.route}
+                                    </Link>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+
+                    <Box sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }}>
+                        <Image src={logo} height={30} width={30} sx={{ display: { xs: 'none', md: 'flex' } }} alt='logo' />
+                    </Box>
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href="#"
+                        className='text-black'
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        SyncHome
+                    </Typography>
+
+                    {/* space */}
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }} />
+
+                    {/* menu For large devices */}
+                    <Box sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page) => (
+                            <Link
+                                className='text-black my-2 mx-1 block uppercase font-mono font-semibold'
+                                href={page?.pathname}
+                                key={page}
+                            >
+                                {page?.route}
+                            </Link>
+                        ))}
+                    </Box>
+
+                    {/* user logo side */}
+                    {user ? <Box sx={{ flexGrow: 0 }}>
+                        <Tooltip title="Open settings">
+                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            id="menu-syncHome"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settings.map((setting) => (
+                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                    <Typography textAlign="center">{setting}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box> :
+                        <Link href={'#'} variant="contained" className='uppercase font-mono font-semibold text-black' color="primary">
+                            Login
+                        </Link>}
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+}
+export default Navbar;

2ba582152b6e1aee86ddd66ac4fdd4835de4d126
MD.SHAHIDUL ISLAM <shahidul.bd.1920@gmail.com>
2024-01-23 02:21:56 +0600

    added smart home section for home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 import HeroSlider from "@/components/HeroSlider/HeroSlider";
+import ManageEverything from "@/components/ManageEverything/ManageEverything";
+import SmartHome from "@/components/SmartHome/SmartHome";
 
 export default function Home() {
   return (
     <main className="min-h-[calc(100vh-64px)]">
       <HeroSlider></HeroSlider>
-      <h1 className='text-4xl text-center mt-10'>Home page</h1>
+      <SmartHome></SmartHome>
+      <ManageEverything></ManageEverything>
     </main>
   )
 }

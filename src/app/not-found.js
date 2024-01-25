46ed7652c45773873e13a4e3da41c316c695f1c5
Md. Neamul Hoqe <hoqe1997@gmail.com>
2024-01-25 19:22:05 +0600

    not found page added

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,8 +1,13 @@
 import Image from "next/image";
 import notFoundBg from '@/assets/syncHome_404.jpg';
+import GoHome from "@/components/Buttons/GoHome";
 
+{/* box tag throw error:  Error: Element type is invalid. Received a promise that resolves to: undefined. Lazy element type must resolve to a class or function. */ }
 export default function NotFound() {
     return <>
-        <Image src={notFoundBg} alt="Not found background image" />
+        <Image src={notFoundBg} alt="Not found background image" width={'100vw'} height={'100vh'} />
+        <div className="text-center w-full -mt-12">
+            <GoHome />
+        </div>
     </>
 }

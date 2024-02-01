5eff1f351493dc9d5c8ec15cbe7539298a349612
rayhan9802 <mdrayhaanahmed@gmail.com>
2024-02-01 15:15:24 +0800

    implement notification page, sign up and sign in

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,15 @@
 import './globals.css'
+import 'react-toastify/dist/ReactToastify.css';
 import { Inter } from 'next/font/google'
 import AuthProvider from '@/provider/AuthProvider'
+import { ToastContainer } from 'react-toastify';
 
-const inter = Inter({ subsets: [ 'latin' ] })
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   applicationName: 'SyncHome',
   publisher: 'Tech Stars',
-  authors: [ { name: 'Md. Neamul Hoqe', url: 'https://www.linkedin.com/in/mneamulh' }, { name: 'Md. Shahidul Islam', url: 'https://www.linkedin.com/in/mneamulh' }, { name: 'Md. Sakibul Hasan', url: 'https://www.linkedin.com/in/mneamulh' }, { name: 'Foysal Rahman', url: 'https://www.linkedin.com/in/mneamulh' }, { name: 'Md. Masud Rana', url: 'https://www.linkedin.com/in/mneamulh' }, { name: 'Rayhan Ahmed', url: 'https://www.linkedin.com/in/mneamulh' } ],
+  authors: [{ name: 'Md. Neamul Hoqe', url: 'https://www.linkedin.com/in/mneamulh' }, { name: 'Md. Shahidul Islam', url: 'https://www.linkedin.com/in/mneamulh' }, { name: 'Md. Sakibul Hasan', url: 'https://www.linkedin.com/in/mneamulh' }, { name: 'Foysal Rahman', url: 'https://www.linkedin.com/in/mneamulh' }, { name: 'Md. Masud Rana', url: 'https://www.linkedin.com/in/mneamulh' }, { name: 'Rayhan Ahmed', url: 'https://www.linkedin.com/in/mneamulh' }],
   title: 'SyncHome',
   description: 'Smart Residential Building Management System',
 }
@@ -17,6 +19,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter?.className} suppressHydrationWarning={true} style={{ margin: 0, padding: 0 }}>
         <AuthProvider>
+          <ToastContainer />
           {children}
         </AuthProvider>
       </body>

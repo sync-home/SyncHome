f12a01e485040ba19b46fcb1869fa17e6cc37d64
Md. Neamul Hoqe <hoqe1997@gmail.com>
2024-02-06 22:08:48 +0600

    [feat](parking): Banner added

--- src/app/(front)/parking/page.js
+++ src/app/(front)/parking/page.js
@@ -1,19 +1,9 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-
+import Banner from "./Banner";
 
 export default function Parking() {
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={2}>
-                <Grid item xs={8}>
-                    <aside>Left side</aside>
-                </Grid>
-                <Grid item xs={4} sx={{ minHeight: '100vh' }}>
-                    <aside>Right side</aside>
-                </Grid>
-            </Grid>
-        </Box>
+        <>
+            <Banner />
+        </>
     );
 }

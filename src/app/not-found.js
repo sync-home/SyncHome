36c8e76e0f836f0f8fbe5502a0a7d202db54ad92
Md. Neamul Hoqe <hoqe1997@gmail.com>
2024-02-07 20:46:50 +0600

    [fix](not-found): Not found page responsive and alignment fixed. Change whole design

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import notFoundBg from '@/assets/syncHome_404.jpg';
 import GoHome from "@/components/Buttons/LinkRedirect";
+import { Typography } from "@mui/material";
 
 export const metadata = {
     title: 'Not Found',
@@ -8,11 +9,13 @@ export const metadata = {
 
 export default function NotFound() {
     return (
-        <section className="max-w-full min-h-screen relative">
-            <Image src={notFoundBg} alt="Not found background image" style={{ maxHeight: '100vh', maxWidth: '100vw' }} />
-            <span className="absolute left-1/2 bottom-0">
-                <GoHome url={'./'} text={'Go Home'} />
+        <section className="max-w-screen w-full max-h-screen h-full overflow-clip">
+            <Image priority={true} src={notFoundBg} alt="Not found background image" style={{ height: '100%', width: '100%' }} />
+            <Typography component={'h1'} className="absolute top-1/4 transform translate-x-1/3 text-white"> <span className="font-mono font-extrabold text-7xl text-yellow-400 drop-shadow-2xl">Oops!</span> <br /> <span className="tracking-[0.75rem] text-xl leading-10">Not Found</span></Typography>
+            <span className="absolute top-1/2 transform translate-x-1/2 translate-y-1/2">
+                <GoHome url={'./'} text={'Go Home'} revert={true} />
             </span>
+            <p className="hidden">Photo by <a href="https://unsplash.com/@claybanks?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Clay Banks</a> on <a href="https://unsplash.com/photos/a-small-cabin-in-the-woods-with-a-fire-pit-JRkXiBmTRDI?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a></p>
         </section>
     )
 }

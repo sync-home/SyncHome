b99dee59a2197d1f14cca2395382e223cdc88d0b
MD.SHAHIDUL ISLAM <shahidul.bd.1920@gmail.com>
2024-02-10 12:43:44 +0600

    [feat](hero slider): explore btn customize

--- src/components/Buttons/LinkRedirect.js
+++ src/components/Buttons/LinkRedirect.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const LinkRedirect = ({ url, text }) => {
     return (
-        <Link href={url} className='btn bg-[#9CA3AF] mt-10 px-10 rounded-full py-3 border-2 border-[#9CA3AF] hover:border-white hover:bg-transparent hover:text-white transition ease-in-out font-bold uppercase'>{text}</Link>
+        <Link style={{color: "#fff"}} href={url} className='btn hover:bg-[#9CA3AF] mt-10 px-10 rounded-full py-3 border-2 hover:border-[#9CA3AF] border-white hover:bg-transparent hover:no-underline transition ease-in-out font-bold uppercase'>{text}</Link>
     );
 };
 
--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -95,7 +95,7 @@ function Navbar() {
     }
 
     return (
-        <AppBar position="static" sx={{ backgroundColor: 'white', color: 'black' }}>
+        <AppBar sx={{ backgroundColor: 'rgba(0,0,0,0.5)', color: 'white', position: 'fixed', top: '0px', left: '0px', zIndex: '100' }}>
             <Container maxWidth="xl">
                 <Toolbar disableGutters>
                     <Box sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }}>
@@ -106,7 +106,7 @@ function Navbar() {
                         noWrap
                         component="a"
                         href='/'
-                        className='text-black'
+                        className='text-white'
                         sx={{
                             mr: 2,
                             display: { xs: 'none', md: 'flex' },
@@ -193,6 +193,7 @@ function Navbar() {
                         {pages?.length && pages.map((page) => (
                             <Box key={page?.route} onClick={() => handleActiveLink(page?.pathname)}>
                                 <Link
+                                // style={{color: 'white'}}
                                     className={`btn ${page?.pathname === activeLink ? 'active' : ''}`}
                                     href={page?.pathname}
 
@@ -241,10 +242,10 @@ function Navbar() {
                             </Menu>
                         </Box> :
                         <Box sx={{ display: 'flex', gap: 1 }}>
-                            <Button variant="outlined" href={'/signin'} className='uppercase font-mono font-semibold text-black whitespace-nowrap' >
+                            <Button variant="outlined" href={'/signin'} className='uppercase font-mono font-semibold text-white whitespace-nowrap' >
                                 Sign In
                             </Button>
-                            <Button variant="outlined" href={'/signup'} className='uppercase font-mono font-semibold text-black whitespace-nowrap' >
+                            <Button variant="outlined" href={'/signup'} className='uppercase font-mono font-semibold text-white whitespace-nowrap' >
                                 Sign Up
                             </Button>
                         </Box> : ''}

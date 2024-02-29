4765b91a9c50bd63e7ad77fd8869344e4ab0ee7e
Md-Neamul-Hoqe <hoqe1997@gmail.com>
2024-02-29 21:03:31 +0600

    [feat](shop): parking banner move to shared component as featuredBanner and made reusable component and used for shop banner

--- src/app/(front)/shop/page.js
+++ src/app/(front)/shop/page.js
@@ -1,8 +1,25 @@
-export default function Shop() {
+import { Box, Typography } from "@mui/material";
+import Image from "next/image";
+import Typewrite from "@/components/Typewrite/Typewrite";
+import bannerBg from '@/assets/shop/smart-control-2.png';
+import featureImg from '@/assets/shop/smart-control-trolly.jpg';
+import FeaturedBanner from "@/components/shared/FeaturedBanner";
 
+
+const title = 'Smart Shopping'
+
+const headings = [
+    'Seamless Smart Control and Remote Shopping!',
+    'Your Pocket`s Command Center for Smart Living!',
+    'Navigate, Shop, and Deliver with a Tap!',
+    'Control, Shop, and Deliver from Anywhere!'
+];
+
+export default function Shop() {
     return (
         <div>
-            <h1 className="text-4xl text-center">Smart Shop</h1>
+            <FeaturedBanner headings={headings} bannerBg={bannerBg} featureImg={featureImg} title={title} />
+
         </div>
     )
 }
